15abbd1a82697788efdd119e8c09dd4a5bc85652
karinamachado <karinamachado@msn.com>
2018-03-05 21:50:30 -0300

    Update 2

--- js/app.js
+++ js/app.js
@@ -1,35 +1,37 @@
-// **** ENEMY Class ****
+// **** Enemy Class ****
 var Enemy = function(x,y) {
-    'use strict';
+   
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
     //x and y coordinates:
-    //this.x = x;
-    //this.y = y;
+     this.x = x;
+     this.y = y;
 
-    //speed of the bugs with Math.random() from
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    //this.speed = Math.floor((Math.random()*200)+100);
+     
+
+    //speed of the bugs - Math.random()
+    this.speed = Math.floor((Math.random()*150)+100);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    'use strict';
+   
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-   /* if(this.x <= 505) {  //canvas.width = 505
+    
+    if(this.x <= 505) {  //width = 505
         this.x = this.x + this.speed * dt;
     } else {
         this.x = -2;
-    }*/
+    }
+
 };
 
-
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen.
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -37,24 +39,25 @@ Enemy.prototype.render = function() {
 // Player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
-    ;
-    this.sprite = 'images/char-boy.png';
+var Player = function(x, y,speed) {
+
     this.x = 200;
     this.y = 400;
-};
+    this.sprite = 'images/char-boy.png';
+
+}
 
 Player.prototype.update = function(dt) {
     ;
 
-    var self = this;
+    var uplayer = this;
     //if left key is pressed:
-    if(this.pressedKey === 'left' && this.x > 0) { //player isn't on left edge
+    if(this.pressedKey === 'left' && this.x > 0) { 
         this.x = this.x - 100;
     }
 
     //if right key is pressed:
-    if(this.pressedKey === 'right' && this.x < 400) { //player isn't on right edge
+    if(this.pressedKey === 'right' && this.x < 400) { 
         this.x = this.x + 100;
     }
 
@@ -68,37 +71,37 @@ Player.prototype.update = function(dt) {
         this.y = this.y + 90;
     }
 
-    //this will make player jump only once when key is pressed:
+    // make player jump only once when key is pressed:
     this.pressedKey = null;
 
-    //if player reaches water, position reset:
+    //if player reaches water, reset:
     if(this.y < 0) {
         this.reset();
     }
 
     allEnemies.forEach(function(enemy) {
-    if(self.x >= enemy.x - 25 && self.x <= enemy.x + 25) {
-        if(self.y >= enemy.y - 25 && self.y <= enemy.y + 25) {
-            self.reset();
+    if(uplayer.x >= enemy.x - 25 && uplayer.x <= enemy.x + 25) {
+        if(uplayer.y >= enemy.y - 25 && uplayer.y <= enemy.y + 25) {
+            uplayer.reset();
             }
         }
     });
 };
 
 Player.prototype.render = function() {
-    'use strict';
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 //handleInput() method for player:
 Player.prototype.handleInput = function(e) {
-    'use strict';
+ 
     this.pressedKey = e;
 };
 
 //Reset player to beginning position
 Player.prototype.reset = function() {
-    'use strict';
+    
    this.x = 200;
    this.y = 400;
 };
@@ -109,10 +112,10 @@ var allEnemies = []; //creates an array of Enemies
 
 //this function will DISPLAY Enemies:
 (function displayEnemies() {
-    'use strict';
+  
     allEnemies.push(new Enemy(0, 50));
-    allEnemies.push(new Enemy(0, 140));
-    allEnemies.push(new Enemy(0, 230));
+    allEnemies.push(new Enemy(0, 100));
+    allEnemies.push(new Enemy(0, 150));
 }());
 
 
